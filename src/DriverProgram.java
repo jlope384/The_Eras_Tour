b2769928e3c12298d189dba0bca2451e5909e1b3
AscencioSIUU <131807749+AscencioSIUU@users.noreply.github.com>
2023-08-18 08:39:09 -0600

    Localidad y DriverProgram actualizados

--- src/DriverProgram.java
+++ src/DriverProgram.java
@@ -4,7 +4,9 @@ public class DriverProgram {
 
     public static void main(String[] args) {
         boolean keeploop = true;
-
+        int totL1 = 0;
+        int totL5 = 0;
+        int totL10 = 0;
         Scanner sc = new Scanner(System.in);
         while (keeploop == true) {
             System.out.println("---------¡BIENVENIDO AL ERAS TOUR!---------");
@@ -17,50 +19,58 @@ public class DriverProgram {
             System.out.println("6. Código Especial");
             System.out.println("7. Salir");
 
-            
             int option = sc.nextInt();
-            
-            switch(option){ // este switch es para que el usuario pueda elegir la opción que desea
 
-                case 1: // este case es para que el usuario pueda ingresar sus datos y se le asigne un ticket
+            switch (option) { // este switch es para que el usuario pueda elegir la opción que desea
+
+                case 1: // este case es para que el usuario pueda ingresar sus datos y se le asigne un
+                        // ticket
                     Comprador comprador = new Comprador();
                     Ticket ticket = new Ticket();
                     comprador.realizarCompra();
-                    System.out.println("ID de ticket: "+comprador.getTicket_id());
-                    if (ticket.validateTicket(comprador.getTicket_id(), 1, 15000)){
+                    System.out.println("ID de ticket: " + comprador.getTicket_id());
+                    if (ticket.validateTicket(comprador.getTicket_id(), 1, 15000)) {
                         System.out.println("Su ticket  es apto para comprar boletos.");
                         comprador.canBuy = true;
-                    }else{
+                    } else {
                         System.out.println("Su ticket no es apto para comprar boletos.");
                         comprador.canBuy = false;
                     }
 
-                    
-                
                     break;
-                case 2: 
+                case 2:
                     continue;
                 case 3:
+                    Localidad localidad = new Localidad();
+                    totL1 = localidad.getTOTLocalidad1();
+                    totL5 = localidad.getTOTLocalidad5();
+                    totL10 = localidad.getTOTLocalidad10();
+
+                    System.out.println("Cantidad total: " + (totL1 + totL5 + totL10));
                     continue;
                 case 4:
+                    System.out.println("Cantidad de asientos en Localidad 1: " + totL1);
+                    System.out.println("Cantidad de asientos en Localidad 5: " + totL5);
+                    System.out.println("Cantidad de asientos en Localidad 10: " + totL10);
                     continue;
                 case 5:
                     continue;
-                case 6: // este case es para que el usuario pueda ingresar un código especial y se le asigne un ticket
+                case 6: // este case es para que el usuario pueda ingresar un código especial y se le
+                        // asigne un ticket
                     System.out.println("Ingrese un número especial: ");
                     int n = sc.nextInt();
-                    if (SpecialTicket.isFibonacci(n)){
+                    if (SpecialTicket.isFibonacci(n)) {
                         System.out.println("El código es correcto. ");
                         System.out.println("Tiene acceso a un espacio en la localidad 10 a $20,000.00");
-                    }else{
+                    } else {
                         System.out.println("El código está incorrecto.");
                     }
 
                 case 7: // este case es para que el usuario pueda salir del programa
                     System.out.println("¡Gracias por usar Eras Tour!");
                     keeploop = false;
-                }
+            }
         }
     }
-    
+
 }
--- src/Localidad.java
+++ src/Localidad.java
@@ -1,24 +1,61 @@
-import java.util.Scanner;
+
 import java.util.Random;
 
 public class Localidad {
-    private int Localidad1 = 100;
-    private int Localidad5 = 500;
-    private int Localidad10 = 1000;
-    
+    private int PrecioLocalidad1 = 100;
+    private int PrecioLocalidad5 = 500;
+    private int PrecioLocalidad10 = 1000;
 
-    public int getLocalidad1() {
-        return this.Localidad1;
+    private int TOTLocalidad1 = 20;
+    private int TOTLocalidad5 = 20;
+    private int TOTLocalidad10 = 20;
+
+    public int getTOTLocalidad1() {
+        return this.TOTLocalidad1;
     }
-    public int getLocalidad5() {
-        return this.Localidad5;
+
+    public int getTOTLocalidad5() {
+        return this.TOTLocalidad5;
     }
-    public int getLocalidad10() {
-        return this.Localidad10;
+
+    public int getTOTLocalidad10() {
+        return this.TOTLocalidad10;
     }
-    public static void SelectLocalidad(){
+
+    public int getPrecioLocalidad1() {
+        return this.PrecioLocalidad1;
+    }
+
+    public int getPrecioLocalidad5() {
+        return this.PrecioLocalidad5;
+    }
+
+    public int getPrecioLocalidad10() {
+        return this.PrecioLocalidad10;
+    }
+
+    public int[] SelectLocalidad(int cantVoletos) {
         Random random = new Random();
-        int RandomNum = random.nextInt(3)+1;
+        int RandomNum = random.nextInt(3) + 1;
         System.out.println("Numero Random: " + RandomNum);
 
+        if (RandomNum == 1) {
+            TOTLocalidad1 = TOTLocalidad1 - cantVoletos;
+            int[] returns1 = { PrecioLocalidad1, TOTLocalidad1 };
+            return returns1;
+        }
+        if (RandomNum == 2) {
+            TOTLocalidad5 = TOTLocalidad5 - cantVoletos;
+            int[] returns5 = { PrecioLocalidad5, TOTLocalidad5 };
+            return returns5;
+        }
+        if (RandomNum == 3) {
+            TOTLocalidad10 = TOTLocalidad10 - cantVoletos;
+            int[] returns10 = { PrecioLocalidad10, TOTLocalidad10 };
+            return returns10;
+        }
+        if (TOTLocalidad1 == 0 || TOTLocalidad5 == 0 || TOTLocalidad10 == 0) {
+            System.out.println("No se encuentran mas asientos disponibles, Buena suerte la proxima ");
+        }
     }
+}
