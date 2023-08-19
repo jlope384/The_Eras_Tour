52582d39c190ac6da812895fdb784adae8ebb6b1
AscencioSIUU <131807749+AscencioSIUU@users.noreply.github.com>
2023-08-18 22:06:14 -0600

    Localidad Actualizada

--- src/Localidad.java
+++ src/Localidad.java
@@ -1,61 +1,70 @@
 import java.util.Random;
 
 public class Localidad {
-    private int PrecioLocalidad1 = 100;
-    private int PrecioLocalidad5 = 500;
-    private int PrecioLocalidad10 = 1000;
+    private static final int PrecioLocalidad1 = 100;
+    private static final int PrecioLocalidad5 = 500;
+    private static final int PrecioLocalidad10 = 1000;
 
     private int TOTLocalidad1 = 20;
     private int TOTLocalidad5 = 20;
     private int TOTLocalidad10 = 20;
+    private int PrecioTOT = 0;
 
     public int getTOTLocalidad1() {
-        return this.TOTLocalidad1;
+        return TOTLocalidad1;
     }
 
     public int getTOTLocalidad5() {
-        return this.TOTLocalidad5;
+        return TOTLocalidad5;
     }
 
     public int getTOTLocalidad10() {
-        return this.TOTLocalidad10;
+        return TOTLocalidad10;
     }
 
     public int getPrecioLocalidad1() {
-        return this.PrecioLocalidad1;
+        return PrecioLocalidad1;
     }
 
     public int getPrecioLocalidad5() {
-        return this.PrecioLocalidad5;
+        return PrecioLocalidad5;
     }
 
     public int getPrecioLocalidad10() {
-        return this.PrecioLocalidad10;
+        return PrecioLocalidad10;
     }
 
-    public int[] SelectLocalidad(int cantVoletos) {
+    public int getPrecioTOT() {
+        return PrecioTOT;
+    }
+
+    public int[] SelectLocalidad(int cantBoletos) {
         Random random = new Random();
         int RandomNum = random.nextInt(3) + 1;
         System.out.println("Numero Random: " + RandomNum);
+        int[] retunrs;
+        if (RandomNum == 1 && TOTLocalidad1 >= cantBoletos) {
+            TOTLocalidad1 = TOTLocalidad1 - cantBoletos; // actualizar cantidad de localizacion
+            PrecioTOT = PrecioLocalidad1 * cantBoletos; // Precio Por cantidad de voletos
+            retunrs = new int[] { PrecioLocalidad1, TOTLocalidad1, PrecioTOT };// sacado por chatGPT, forma de hacer
+                                                                               // return
+        }
 
-        if (RandomNum == 1) {
-            TOTLocalidad1 = TOTLocalidad1 - cantVoletos;
-            int[] returns1 = { PrecioLocalidad1, TOTLocalidad1 };
-            return returns1;
+        else if (RandomNum == 2 && TOTLocalidad5 >= cantBoletos) {
+            TOTLocalidad5 = TOTLocalidad5 - cantBoletos;// actualizar cantidad de localizacion
+            PrecioTOT = PrecioLocalidad5 * cantBoletos; // Precio Por cantidad de voletos
+            retunrs = new int[] { PrecioLocalidad5, TOTLocalidad5 };// sacado por chatGPT, forma de hacer return
         }
-        if (RandomNum == 2) {
-            TOTLocalidad5 = TOTLocalidad5 - cantVoletos;
-            int[] returns5 = { PrecioLocalidad5, TOTLocalidad5 };
-            return returns5;
+
+        else if (RandomNum == 3 && TOTLocalidad10 >= cantBoletos) {
+            TOTLocalidad10 = TOTLocalidad10 - cantBoletos;// actualizar cantidad de localizacion
+            PrecioTOT = PrecioLocalidad10 * cantBoletos; // Precio Por cantidad de voletos
+            retunrs = new int[] { PrecioLocalidad10, TOTLocalidad10 };// sacado por chatGPT, forma de hacer return
+        } else {
+            System.out.println("No hay asientos disponibles.");
+            retunrs = new int[] { 0, 0 }; // O lanza una excepción
         }
-        if (RandomNum == 3) {
-            TOTLocalidad10 = TOTLocalidad10 - cantVoletos;
-            int[] returns10 = { PrecioLocalidad10, TOTLocalidad10 };
-            return returns10;
-        }
-        if (TOTLocalidad1 == 0 || TOTLocalidad5 == 0 || TOTLocalidad10 == 0) {
-            System.out.println("No se encuentran mas asientos disponibles, Buena suerte la proxima ");
-        }
+        return retunrs; // forma de hacer return sacada por ChatGPT
     }
 
     public void comprarBoletos(int nBoletos, int i) {
