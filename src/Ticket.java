7b9f5b7acad0f53f50d65bc9bd74352a62e482c4
AscencioSIUU <131807749+AscencioSIUU@users.noreply.github.com>
2023-08-18 23:33:54 -0600

    COMMIT "LA FINALISIMA"

--- src/Ticket.java
+++ src/Ticket.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 public class Ticket {
 
     private static int ticket_id;
-    
-    public  boolean validateTicket(int ticket, int min, int max){ // este método es para validar el ticket
+
+    public boolean validateTicket(int ticket, int min, int max) { // este método es para validar el ticket
         Random rand = new Random();
         int[] rango = new int[2];
         min = rand.nextInt((15000));
@@ -14,18 +14,13 @@ public class Ticket {
         rango[1] = max;
         Arrays.sort(rango); // se agregan ambos valores a un array y se ordenan de menor a mayor
 
-
-        if(ticket_id >= rango[0] && ticket_id <= rango[1]){
-            return true; // si el ticketid está entre el rango de min y max, entonces es válido y retorna true
-        }
-        else{
+        if (ticket_id >= rango[0] && ticket_id <= rango[1]) {
+            return true; // si el ticketid está entre el rango de min y max, entonces es válido y retorna
+                         // true
+        } else {
             return false; // si no, retorna false
         }
-        
+
     }
 
-
-
-
-    
 }
