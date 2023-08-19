19bf40e9c3ef37d015576d11d1abd23965f39c4d
jlope384 <lop23415@uvg.edu.gt>
2023-08-18 21:22:21 -0600

    a

--- src/vender.java
+++ src/vender.java
@@ -7,7 +7,22 @@ public class vender {
         this.presupuesto = presupuesto;
     }
 
-private int getTicketCount() {
-    return ticketCount;
+    private int getTicketCount() {
+        return ticketCount;
 
+    }
+
+    private int getPresupuesto() {
+        return presupuesto;
+    }
+
+    private boolean validateTicket(int ticketCount, int presupuesto) {
+        if (ticketCount <= 0) {
+            return false;
+        }
+        if (presupuesto <= 0) {
+            return false;
+        }
+        return true;
+    }
 }
